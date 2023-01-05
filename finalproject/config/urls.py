5719f7f231257f84c583089868b53f493009be33
platypus46 <svolts2185@gmail.com>
2023-01-05 11:57:08 +0900

    setting

--- finalproject/config/urls.py
+++ finalproject/config/urls.py
@@ -1,7 +1,7 @@
-"""finalproject URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from b4 import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('b4/',views.test),
 ]
