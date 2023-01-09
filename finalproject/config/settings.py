af23662b6e28ae161473b5dbfd54ad924f74da5a
somi <joosomi@naver.com>
2023-01-10 00:56:55 +0900

    share page html

--- finalproject/config/settings.py
+++ finalproject/config/settings.py
@@ -122,7 +122,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIR = [BASE_DIR / 'static']
