f168beb7c3ed591b6769f7e5f12c5e93b22823d8
somi <joosomi@naver.com>
2023-01-10 21:02:15 +0900

    start_page 수정

--- finalproject/b4/static/b4/js/start.js
+++ finalproject/b4/static/b4/js/start.js
@@ -1,7 +1,23 @@
 const camera = document.querySelector(".camera");
 const gallery = document.querySelector(".gallery");
 
+const pop_up_bg = document.querySelector(".pop_up_bg");
+
+
+
 function setChildValue(index1) {
     window.location.href = `/${index1}`;
+    // window.location.href = `indexcopy.html?${index1}?${index2}`;
+}
+
+function popup_open() {
+    document.querySelector("#pop_up_bg").style.display = 'flex';
+
+    // window.location.href = `indexcopy.html?${index1}?${index2}`;
+}
+
+function popup_close() {
+    document.querySelector("#pop_up_bg").style.display = 'none';
+
     // window.location.href = `indexcopy.html?${index1}?${index2}`;
 }
