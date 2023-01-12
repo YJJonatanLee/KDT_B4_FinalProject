4f097deeb929b436bea0a968c96ac263a6c0d403
platypus46 <svolts2185@gmail.com>
2023-01-12 16:45:07 +0900

    model_serving

--- finalproject/b4/admin.py
+++ finalproject/b4/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Photos
+from .models import Photos,Test
 
 admin.site.register(Photos)
+admin.site.register(Test)
 # Register your models here.
--- finalproject/b4/views.py
+++ finalproject/b4/views.py
@@ -1,14 +1,41 @@
 import base64
+import cv2
+import numpy as np
+import os
+
+def make_file_list(path_dir):
+    file_list = os.listdir(path_dir)
+    file_name_list = []
+    for i in range(len(file_list)):
+        file_name_list.append(file_list[i].replace(".jpg",""))
+    return file_list, file_name_list
+
+def Cutting_face_save(image, name, saving_dir):
+    face_cascade = cv2.CascadeClassifier('media/haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cropped = image[y-40 : y+h+40, x-40: x+w+40]
+        resize = cv2.resize(cropped, (180,180))
+        cv2.imwrite(os.path.join(saving_dir, f"{name}.jpg"), resize)
+
+
+path_dir = 'media/origin/'
+saving_dir = 'media/cutting_faces/'
+files, file_names = make_file_list(path_dir)
+
+
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Photos
+from .models import Photos,Test
+
 
 def test(request):
     return render(request,'camera_view.html')
-# Create your views here.
 
 
 def loading(request):
+    Cutting_face_save(cv2.imread(os.path.join(path_dir, files[0])), file_names[0], saving_dir)
     return render(request,'loading.html')
 
 def bg_color(request):
@@ -50,3 +77,10 @@ def django_html_webcam_image_upload(request):
         with open('static/b4/img/image.png', 'wb') as fh:
             fh.write(base64.b64decode(img_data))
         return HttpResponse('Image uploaded successfully!')
+
+
+
+
+
+
+    
