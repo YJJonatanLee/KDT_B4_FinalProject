e445ebaa86b0d5207e91116c9e61eb037d640f99
platypus46 <svolts2185@gmail.com>
2023-01-16 22:56:54 +0900

    start_page+camera

--- finalproject/b4/static/b4/js/start.js
+++ finalproject/b4/static/b4/js/start.js
@@ -1,9 +1,43 @@
 const camera = document.querySelector(".camera");
 const gallery = document.querySelector(".gallery");
-
 const pop_up_bg = document.querySelector(".pop_up_bg");
 
+const video = document.getElementById('video');
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+      
+navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+    video.srcObject = stream;
+});
+      
+function captureImage() {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    context.drawImage(video, 0, 0);
+    canvas.toBlob(blob => {
+        const formData = new FormData();
+        formData.append('camera-image', blob);
+        fetch('/test/', {
+            method: 'POST',
+            body: formData
+        });
+    });
+}
 
+//임의로 넣어둠.
+function captureImage() {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    context.drawImage(video, 0, 0);
+    canvas.toBlob(blob => {
+      const formData = new FormData();
+      formData.append('camera-image', blob);
+      fetch('/test/', {
+        method: 'POST',
+        body: formData
+      });
+    });
+  }
 
 function setChildValue(index1) {
     window.location.href = `/${index1}`;
