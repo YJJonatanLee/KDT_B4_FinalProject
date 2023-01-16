23dc3323036ba79c69100d6b6e6f7d67501e7d86
somi <joosomi@naver.com>
2023-01-17 00:29:29 +0900

    배경색 변경 js 추가

--- finalproject/b4/models.py
+++ finalproject/b4/models.py
@@ -5,7 +5,7 @@ class Photos(models.Model):
     # models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     origin_photo = models.ImageField(upload_to="origin",null=True)
     converte_photo = models.ImageField(upload_to="converte")
-    background_color = models.CharField(default='',max_length=3)
+    background_color = models.CharField(default='',max_length=10)
     background_photo = models.ImageField(upload_to="background")
 
 class CameraImage(models.Model):
--- finalproject/b4/static/b4/js/bg_color.js
+++ finalproject/b4/static/b4/js/bg_color.js
@@ -1,5 +1,14 @@
-function go2share() {
-    // document.querySelector("#pop_up_bg").style.display = 'flex';
-    window.location.href = `/share/1`;
-    // window.location.href = `indexcopy.html?${index1}?${index2}`;
-}
+// const bg_img = document.querySelector(".bg_img");
+
+// function go2share() {
+//     // document.querySelector("#pop_up_bg").style.display = 'flex';
+//     window.location.href = `/share/1`;
+//     // window.location.href = `indexcopy.html?${index1}?${index2}`;
+// }
+
+// function changeBg(color){
+//     bg_img.style.backgroundColor = color;
+//     // if (color==="1"){
+//     //     bg_img.style.backgroundColor =
+//     // }
+// }
