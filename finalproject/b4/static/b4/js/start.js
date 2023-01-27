3828bf45a69ba92322ae0d4f4f8499572631ad7f
somi <joosomi@naver.com>
2023-01-27 16:22:43 +0900

    start.js 주석 추가 및 코드 정리

--- finalproject/b4/static/b4/js/start.js
+++ finalproject/b4/static/b4/js/start.js
@@ -1,8 +1,3 @@
-// const camera = document.querySelector(".camera");
-// const gallery = document.querySelector(".gallery");
-
-const pop_up_bg = document.querySelector(".pop_up_bg");
-
 const video = document.getElementById('video');
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
@@ -12,7 +7,7 @@ const camera_btn = document.querySelector(".camera_btn");
 const gallery_btn = document.querySelector(".gallery_btn");
 const guideline = document.querySelector(".guideline");
 
-document.getElementById('fileinput').addEventListener('change', function(evt) {
+document.getElementById('fileinput').addEventListener('change', function (evt) {
     var tgt = evt.target || window.event.srcElement,
         files = tgt.files;
     if (FileReader && files && files.length) {
@@ -28,15 +23,20 @@ navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
     video.srcObject = stream;
 });
 
+
+// 로딩창 실행 및 다음 페이지로 이동 함수
 function setChildValue(index1) {
     //메시지는 임의로 설정했습니다.
-    if(camera_btn.innerText === '촬영'){
+    if (camera_btn.innerText === '촬영') {
         alert("사진을 선택해주세요.");
     }
-    else{
+    else {
+        // 로딩 창 실행
         document.querySelector('.main').classList.add('display-none');
         document.querySelector('.main2').classList.remove('display-none');
         setTimeout(typing, 1500);
+
+
         var imageSrc = document.getElementById('image').src;
         var binary = atob(imageSrc.split(',')[1]);
         var array = [];
@@ -50,11 +50,12 @@ function setChildValue(index1) {
             method: 'POST',
             body: formData
         });
+
+        // 15초 후 페이지 이동
         setTimeout(function () {
-            window.location.href = "/color";
+            window.location.href = `/${index1}`;
         }, 15000);
-        // window.location.href = `/${index1}`;
-    } 
+    }
 }
 
 
@@ -66,49 +67,35 @@ function camera() {
         context.drawImage(video, 0, 0);
         const imageData = canvas.toDataURL('image/png');
         image.src = imageData;
-
         camera_btn.innerText = '재촬영';
-        guideline.style.display =  "none";
-        video.style.direction='none';
+        guideline.style.display = "none";
+        video.style.direction = 'none';
 
-        image.style.display="block";
+        image.style.display = "block";
 
-        
-    }else if (camera_btn.innerText === '재촬영'||camera_btn.innerText === '카메라') {
+
+    } else if (camera_btn.innerText === '재촬영' || camera_btn.innerText === '카메라') {
         camera_btn.innerText = '촬영';
-        guideline.style.display =  "block";
-        video.style.direction='block';
+        guideline.style.display = "block";
+        video.style.direction = 'block';
 
-        image.style.display="none";  
+        image.style.display = "none";
 
     }
 }
 
 function gallery() {
     camera_btn.innerText = '카메라';
-    guideline.style.display =  "none";
-    image.style.display="block";
+    guideline.style.display = "none";
+    image.style.display = "block";
 
 }
 
-// function popup_open() {
-//     document.querySelector("#pop_up_bg").style.display = 'flex';
-
-//     // window.location.href = `indexcopy.html?${index1}?${index2}`;
-// }
-
-// function popup_close() {
-//     document.querySelector("#pop_up_bg").style.display = 'none';
-
-//     // window.location.href = `indexcopy.html?${index1}?${index2}`;
-// }
 
 
-// $(document)
-
+// 로딩창 텍스트 애니메이션
 
 const text = document.querySelector(".str");
-// text.innerHTML = 'asdfasdf'; 
 
 // 글자 모음
 const letters = ["촬영 시작합니다.", "앞에 봐주세요~", "얼굴 살짝만 왼쪽으로", "한 번 더 찍을게요."];
@@ -118,43 +105,38 @@ const speed = 200;
 let i = 0;
 
 // 타이핑 효과
-const typing = async () => {  
-  const letter = letters[i].split("");
-  
-  while (letter.length) {
-    await wait(speed);
-    text.innerHTML += letter.shift(); 
-  }
-  
-  // 잠시 대기
-  await wait(800);
-  
-  // 지우는 효과
-  remove();
+const typing = async () => {
+    const letter = letters[i].split("");
+
+    while (letter.length) {
+        await wait(speed);
+        text.innerHTML += letter.shift();
+    }
+
+    // 잠시 대기
+    await wait(800);
+
+    // 지우는 효과
+    remove();
 }
 
 // 글자 지우는 효과
 const remove = async () => {
-  const letter = letters[i].split("");
-  
-  while (letter.length) {
-    await wait(speed);
-    
-    letter.pop();
-    text.innerHTML = letter.join(""); 
-  }
-  
-  // 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
-  i = !letters[i+1] ? 0 : i + 1;
-  typing();
+    const letter = letters[i].split("");
+
+    while (letter.length) {
+        await wait(speed);
+
+        letter.pop();
+        text.innerHTML = letter.join("");
+    }
+
+    // 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
+    i = !letters[i + 1] ? 0 : i + 1;
+    typing();
 }
 
 // 딜레이 기능 ( 마이크로초 )
 function wait(ms) {
-  return new Promise(res => setTimeout(res, ms))
+    return new Promise(res => setTimeout(res, ms))
 }
-
-// 초기 실행
-
-
-
