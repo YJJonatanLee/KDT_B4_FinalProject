85e16ec2b5d8b8809f50fe9cfcc7869a8ac682cf
somi <joosomi@naver.com>
2023-02-28 22:37:52 +0900

    필요없는 파일 삭제

--- finalproject/b4/models.py
+++ finalproject/b4/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Photos(models.Model):
     id = models.AutoField(primary_key=True)
     uuid = models.CharField(default='',max_length=100,null=True)
-    # models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     origin_photo = models.ImageField(upload_to="origin",null=True)
     converted_photo = models.ImageField(upload_to="converted",null=True)
     background_color = models.CharField(default='#ffffff',max_length=10,null=True)
